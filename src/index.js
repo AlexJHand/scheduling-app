7a92d76c0a334355b7fc4c99811c4a0a0fd6c85e
Alex Hand <alex.j.hand@gmail.com>
2018-05-22 17:31:11 -0500

    Log added to show where client-side js is

--- src/index.js
+++ src/index.js
@@ -6,3 +6,5 @@ import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+console.log('In js'); // Comment used to show me that this is the client-side js file.
